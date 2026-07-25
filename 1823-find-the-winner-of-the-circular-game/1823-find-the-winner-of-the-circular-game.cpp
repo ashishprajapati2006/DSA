558972b8b9bef25750c927b20a6f195b60af6eae
ASHISH PRAJAPATI <ashishofficial12321@gmail.com>
2026-07-25 13:52:25 +0530

    Time: 0 ms (100%), Space: 8 MB (65.26%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
+    int winner(int n, int k){
+        if(n==1)
+        return 0;
 
-    int winner(vector<bool>&person, int n, int index, int person_left, int k){
-
-        // base condition
-        if(person_left==1){
-            for(int i=0; i<n; i++){
-                if(person[i]==0)
-                return i;
-            }
-        }
-        // find the posion to kill
-        int kill = (k-1)% person_left;
-
-        while(kill--){
-            index = (index+1)% n;  // taki rotaion m shi rhe
-            while(person[index]==1)
-            index = (index+1)%n;  // killed person ko skip kerenge
-        }
-        person[index]=1;
-
-        //next alive person
-        while(person[index]==1){
-            index=(index+1)%n;
-        }
-        
-        return winner(person, n, index, person_left-1, k);
+        return (winner(n-1, k)+k)% n;
     }
-
     int findTheWinner(int n, int k) {
-        vector<bool>person(n,0);
-        return winner(person,n,0,n,k)+1;
+        return winner(n,k)+1;
         
     }
 };
